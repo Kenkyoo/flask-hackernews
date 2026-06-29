3c41a2edc1f5d2e5e397ddb63952d72428e04c1e
Franco <frfantusatti@protonmail.com>
2026-06-29 18:52:06 -0300

    create app flask

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Author(db.Model):
+    author_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    news = db.relationship("New", backref="author")
+
+    def __repr__(self):
+        return '<Author: {}>'.format(self.news)
+
+class New(db.Model):
+    new_id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.new_id"))
+    title = db.Column(db.String)
